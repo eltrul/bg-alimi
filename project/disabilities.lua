eaf7f07b59c26d5ab7555ef71a0b023212a9d15c
Ishikawa <105537798+zaizooo@users.noreply.github.com>
2023-08-13 12:24:31 +0700

    Update disabilities.lua

--- project/disabilities.lua
+++ project/disabilities.lua
@@ -1,4 +1,4 @@
 
 
 print("Ok Loaded") 
-loadstring(game:HttpGet"https://raw.githubusercontent.com/zaizooo/ayumi-cdn/main/project/Protected_7955797178735304.lua.txt")()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/zaizooo/ayumi-cdn/main/project/Protected_8889114376744234.lua.txt")()
